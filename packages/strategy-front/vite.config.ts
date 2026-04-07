8ea94d81c88347c74f5536ad742cb05bfa4ed3b6
guhl <89780012@qq.com>
2026-04-07 09:35:44 +0800

    fix:  解决react 、react-dom、react-redux 进使用包内依赖

--- packages/strategy-front/vite.config.ts
+++ packages/strategy-front/vite.config.ts
@@ -8,13 +8,18 @@ export default defineConfig(({ command, mode }) => {
   const apiProxyTarget = env.VITE_API_PROXY_TARGET || "http://127.0.0.1:8000"
   const opencodeProxyTarget = env.VITE_OPENCODE_PROXY_TARGET || apiProxyTarget
   const isDevelopMode = mode === "development"
+  const root = path.resolve(__dirname, "./node_modules")
 
   return {
     plugins: [react(), tailwindcss()],
     resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "./src"),
-      },
+      alias: [
+        { find: "@", replacement: path.resolve(__dirname, "./src") },
+        { find: "react", replacement: path.resolve(root, "./react") },
+        { find: "react-dom", replacement: path.resolve(root, "./react-dom") },
+        { find: "react-redux", replacement: path.resolve(root, "./react-redux") },
+      ],
+      dedupe: ["react", "react-dom", "react-redux"],
     },
     server:
       command === "serve" && isDevelopMode
